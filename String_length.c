27cf643c8eb00b65d17e8cf7e1fc289efd31ff26
BiditaPaul <66505690+BiditaPaul@users.noreply.github.com>
2020-06-30 12:26:47 +0530

    Update String_length.c

--- String_length.c
+++ String_length.c
@@ -4,13 +4,11 @@ int main()
 {
     char *arr;
     int length=0;
-    int i=0;
     printf("Enter a string: ");
     scanf("%s",arr);
-    while(*(arr+i)!='\0')
+    while(*(arr+length)!='\0')
     {
         length = length + 1;
-        i++;
     }
     printf("length of string '%s' is : %d",arr,length);
 
